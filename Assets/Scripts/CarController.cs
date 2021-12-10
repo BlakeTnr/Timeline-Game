f34a36c4efec29febde67db2b70c96d1539301a0
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-10 14:53:51 -0500

    Car Movement

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -9,7 +9,7 @@ public class CarController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,22 +17,47 @@ public class CarController : MonoBehaviour
     {
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         carControllerUpdate();
     }
 
     public float maxAcceleration;
-    private void carControllerUpdate() {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+    public float maxTurningAmount;
 
-        rigidbody.AddForce(new Vector3(0, 0, vertical*maxAcceleration));
+    private float horizontalInput;
+    private float verticalInput;
+    private void carControllerUpdate()
+    {
+        updateInputs(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        handleAccelerating();
+        handleTurning();
+    }
+
+    private void handleAccelerating()
+    {
+        Vector3 worldFromLocalAcceeleration = transform.TransformVector(new Vector3(0, 0, verticalInput * maxAcceleration));
+        rigidbody.AddForce(worldFromLocalAcceeleration);
         capSpeed();
     }
 
+    public void updateInputs(float horizontal, float vertical)
+    {
+        this.horizontalInput = horizontal;
+        this.verticalInput = vertical;
+    }
+
+    private void handleTurning()
+    {
+        gameObject.transform.Rotate(new Vector3(0, horizontalInput * maxTurningAmount, 0));
+    }
+
     public float maxSpeed;
-    private void capSpeed() {
-        if(rigidbody.velocity.z > maxSpeed) {
+    private void capSpeed()
+    {
+        if (rigidbody.velocity.z > maxSpeed)
+        {
             float preserveSign = Mathf.Sign(rigidbody.velocity.z);
             Vector3 velocity = rigidbody.velocity;
             velocity.z = preserveSign * maxSpeed;
