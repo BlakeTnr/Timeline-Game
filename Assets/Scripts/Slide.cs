0377ce1859878667b84344b452ce7009e1c22e58
Blake <80996688+BlakeTnr@users.noreply.github.com>
2021-12-12 14:27:48 -0500

    Perception System

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -16,7 +16,6 @@ public class Slide
 
     public void display()
     {
-        Debug.Log(slidePrefab);
         this.slide = GameObject.Instantiate(slidePrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
         setAllData();
     }
